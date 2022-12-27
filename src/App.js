579efd5835c2aec28c39d3fa49795c0a4c17b15e
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2022-12-27 14:25:50 +0200

    feat: implement the Login component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import MainHeader from "./components/MainHeader/MainHeader";
+import Login from "./components/Login/Login";
 
 const App = () => {
   return (
     <>
       <MainHeader />
+      <main>
+        <Login />
+      </main>
     </>
   );
 };

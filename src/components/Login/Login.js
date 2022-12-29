4ede6926707f06d09694a4adf7723c606d379a2f
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2022-12-29 22:04:54 +0200

    feat: implement input focusing during validation via forwardRef

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import { useState, useReducer, useContext, useEffect } from "react";
+import { useState, useReducer, useContext, useEffect, useRef } from "react";
 
 import AuthContext from "../../store/auth-context";
 import Card from "../UI/Card/Card";
@@ -48,6 +48,9 @@ const Login = () => {
 
   const authCtx = useContext(AuthContext);
 
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
   useEffect(() => {
     const identifier = setTimeout(() => {
       console.log("Checking form validity!");
@@ -78,13 +81,21 @@ const Login = () => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, passwordState.value);
+
+    if (formIsValid) {
+      authCtx.onLogin(emailState.value, passwordState.value);
+    } else if (!emailIsValid) {
+      emailInputRef.current.focus();
+    } else {
+      passwordInputRef.current.focus();
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+          ref={emailInputRef}
           id="email"
           label="E-Mail"
           title="email"
@@ -94,6 +105,7 @@ const Login = () => {
           onBlur={validateEmailHandler}
         />
         <Input
+          ref={passwordInputRef}
           id="password"
           label="Password"
           title="password"
@@ -103,7 +115,7 @@ const Login = () => {
           onBlur={validatePasswordHandler}
         />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
